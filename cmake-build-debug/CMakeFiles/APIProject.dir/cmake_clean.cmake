c365ca5d6eb1e0fb5f39487594ae578ae1435767
Unknown <marco.mandelli212@gmail.com>
2019-07-24 11:17:29 +0200

    Init

---
+++ cmake-build-debug/CMakeFiles/APIProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "APIProject"
+  "APIProject.pdb"
+  "CMakeFiles/APIProject.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/APIProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
